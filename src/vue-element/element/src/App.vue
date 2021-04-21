f1eb02bc0ff396cdea20c9548bc5d80526f6ec2c
lanyuechuan <lanyuechun592@163.com>
2021-04-21 23:30:46 +0800

    fix: 前后端分离前端基本页面搭建

--- src/vue-element/element/src/App.vue
+++ src/vue-element/element/src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <a href="#/button">点我显示button</a>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <router-view/>
+    <el-container>
+    <el-header>
+        <!-- 导航菜单 -->
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="/">主页</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">填志愿</template>
+                <el-menu-item index="2-1">院校优先</el-menu-item>
+                <el-menu-item index="2-2">专业优先</el-menu-item>
+                <el-menu-item index="2-3">测录取概率</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="3">找学校</el-menu-item>
+            <el-menu-item index="4">找专业</el-menu-item>
+            <el-menu-item index="/user">用户中心</el-menu-item>
+            <el-menu-item index="/login">登录</el-menu-item>
+            <el-menu-item index="/register">注册</el-menu-item>
+        </el-menu>
+    </el-header>
+        <el-main>
+            <router-view/>
+        </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+        return {
+                activeIndex: '1',
+                activeIndex2: '1'
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+                this.$router.push(key);
+            }
+        }
 }
 </script>
 
