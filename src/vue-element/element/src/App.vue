796d05fd344bdc3a6f2708abeaec60907a9f64b6
lanyuechuan <lanyuechun592@163.com>
2021-04-21 08:28:59 +0800

    feat: 前后端分离，vue脚手架结合vue框架的elementui搭建前端页面

---
+++ src/vue-element/element/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <a href="#/button">点我显示button</a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
